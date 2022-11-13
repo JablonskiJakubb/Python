cdf33d9570a3e123be49120a2db30ef9d767540a
postaram się wylogować <114925147+Jacob2138@users.noreply.github.com>
2022-11-14 00:29:58 +0100

    Create karta pracy 2a.py

---
+++ karta pracy 2a.py
@@ -0,0 +1,53 @@
+import math
+# # zad 1
+#
+# a = int(input())
+# b = int(input())
+#
+# if (a+b)%2==0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# zad 2
+# a = int(input())
+# g = int(input())
+#
+# if (a + g)/2> math.sqrt(a*g):
+#     print("arytmetyczna większa")
+# else:
+#     print("geometryczna większa")
+
+# zad 3
+# k = int(input())
+# l = int(input())
+# m = int(input())
+
+# if (k==l and m!=k and m!=l):
+#     print ("TAK równe są ", k, "i", l)
+# else:
+#     if (k == m and l != m and k != l):
+#         print("Tak równe są", k, "i", m)
+#     else:
+#         if (m == l and k != m and k != l):
+#             print("Tak równe są", m, "i", l)
+#         else:
+#             if (k != l and l != m and k != m):
+#                 print("żadna nie jest równa")
+#
+# k = int(input())
+# l = int(input())
+# m = int(input())
+#
+# if (k == l and m != k and m != l):
+#     print("TAK równe są ", k, "i", l)
+# else:
+#     if (k != l and m == k and m != l):
+#         print("TAK, równe są ", m, "i", k)
+#     else:
+#         if (k != l and m != k and m == l):
+#             print("TAK, równe są ", m, "i", l)
+#         else:
+#             print("NIE, żadna z nich nie jest równa lub wszystkie 3 są równe")
+#
+#
