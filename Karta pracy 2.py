84c6b6cca1c699569e36ff2d367f33aa52f400bd
postaram się wylogować <114925147+Jacob2138@users.noreply.github.com>
2022-11-14 00:29:18 +0100

    Create Karta pracy 2.py

---
+++ Karta pracy 2.py
@@ -0,0 +1,55 @@
+# zad 1
+
+# n = int(input())
+# if n  %  3 == 0:
+#     print("tak")
+# else:
+#     print("nie")
+
+# zad 2
+# n = int(input())
+# if n > 99 and n < 1000 and n % 17 ==0:
+#     print("tak")
+# else:
+#     print("nie")
+
+# # zad 3
+# n = int(input())
+# if n >=18:
+#     print("pełnoletni")
+# else:
+#     print("niepełnoletni")
+
+# # zad 4
+# n = int(input())
+# LIMIT = 20
+# if LIMIT <n:
+#     print("powyżej")
+# else:
+#     print("poniżej")
+
+# zad 5
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a<c<b:
+#     print("mieści się")
+# else:
+#     print("nie mieści się")
+
+# zad 6
+# a = int(input("podaj dowolną liczbę"))
+# p = int(input("podaj dowolną liczbę pierwszą"))
+# if ((a**p - a) % p ==0):
+#     print("spełnia")
+# else:
+#     print("nie spełnia")
+
+# zad 7
+# p = int(input())
+# k = int(input())
+# s = int(input())
+# if k-p < 3*s:
+#     print ("przeskoczy")
+# else:
+#     print ("zatonie")
