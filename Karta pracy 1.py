8e06c95deda69778863e545caabbac883c89f9bb
postaram się wylogować <114925147+Jacob2138@users.noreply.github.com>
2022-11-14 00:28:10 +0100

    Create Karta pracy 1.py

---
+++ Karta pracy 1.py
@@ -0,0 +1,74 @@
+# # Zad 1
+# a = int(input())
+# b = int(input())
+# print(a**2 + b**2)
+
+# # Zad 2
+# a = int(input())
+# b = int(input())
+# print ((a+b)**2)
+
+# # Zad 3
+# a = int(input())
+# b = int(input())
+# print ((a-b)**3)
+
+# # Zad 4
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print (a*b*c)
+
+# # Zad 5
+# a = int(input())
+# b = int(input())
+# print(2*(a+b)/5)
+
+# # Zad 6
+# brutto = int(input())
+# print(brutto / 1,23)
+
+# # Zad 7
+# a = int(input())
+# b = int(input())
+# print (a%b)
+
+a = int(input("podaj liczbe czarnuch"))
+for i in range (0,a,1):
+    print("czarnuch")
+
+#próba 1
+#zad 1
+# a = int(input())
+# b = int(input())
+# print (a**2 + b**2)
+
+#zad 2
+# a = int(input())
+# b = int(input())
+# print ((a + b)**2)
+
+#zad 3
+# a = int(input())
+# b = int(input())
+# print ((a - b)**3)
+
+#zad 4
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print (a*b*c)
+
+#zad 5
+# a = int(input())
+# b = int(input())
+# print (2*(a+b)/5)
+
+# zad 6
+# brutto = int(input())
+# print (brutto/1,23)
+
+# # zad 7
+# a = int(input())
+# b = int(input())
+# print (a%b)
