86f3ec41cc94995d1cf513aa753d0298e4bd37c9
postaram się wylogować <114925147+Jacob2138@users.noreply.github.com>
2022-12-14 15:09:51 +0100

    Create karta pracy 6.py

---
+++ karta pracy 6.py
@@ -0,0 +1,50 @@
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a<b or a == b & b<c or b == c:
+#     print("Rosnący")
+# else:
+#     print("Malejący")
+#
+# if a-b == b-c:
+#     print("arytmetyczny")
+# elif a / b == b/c:
+#     print("geometrzyczny")
+
+# suma = 0
+# for i in range (99,999):
+#     if i % 8==0 and i % 16 != 8:
+#         suma = suma + i
+# print(suma)
+
+# ilosc = 0
+# wielok = 0
+# for i in range(99,9,-1):
+#     if i % 7 ==0:
+#         wielok = i
+#         break
+# ilosc = 0
+# for i in range(1000, 10000):
+#     if i %wielok==0:
+#         ilosc = ilosc+i
+# print(ilosc)
+# ilosc = 0
+# for i in range(10,100):
+#     if i // 10 >=2 * (i%10):
+#         ilosc += i
+# print(ilosc)
+
+# zad 6
+# suma = 0
+# n = int(input())
+# for i in range (10,n*19):
+#     if i%19==0:
+#         suma = suma + i
+# print(suma)
+
+# from math import pow
+# n = int(input())
+# suma = 0
+# for i in range(n):
+#     suma=suma+(3*i*2) * pow(-1,i)
+# print(suma)
