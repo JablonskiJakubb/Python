e5d3f7601d96bf7ff22fcdd67ed0d0584144d97b
Lepiej się postaraj bo inaczej (Yo to wolter łajt) <114925147+Jacob2138@users.noreply.github.com>
2023-01-02 14:14:42 +0100

    Create 6a.py

---
+++ 6a.py
@@ -0,0 +1,14 @@
+# #zad 11
+
+# n = int(input())
+# for i in range(1, n+1):
+#     suma = suma + (2*i-1)/(i**2)
+# suma = 0
+# print(suma)
+
+#  zad 13
+# suma = 0
+# n = int(input())
+# for i in range(1, n+1):
+#     suma = suma + (2*i)/(i**3 + 2)
+# print(suma)
